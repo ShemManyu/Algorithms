d86c26857fdfc29007a035791a4ee5f3246c8fdb
ShemManyu <smanyu48@gmail.com>
2017-10-18 20:06:39 +0300

    Created folder for compression algorithms

--- Compression/huffman.py
+++ Compression/huffman.py
@@ -1,8 +1,9 @@
 from collections import Counter
+import copy
 
 #Structure to represent nodes in a tree
 #Letter is optional value since merging of two nodes will only hod a count value 
-class Node:
+class HuffNode:
     def __init__(self, count, letter=None):
         self.letter = letter
         self.count = count
@@ -13,16 +14,33 @@ word = input()
 d = dict(Counter(word))
 
 #List of nodes that is sorted based on word count from most frequent to least frequent
-Nodes = [Node(d[w], w) for w in sorted(d, key=d.get, reverse=True)]
+Nodes = [HuffNode(d[w], w) for w in sorted(d, key=d.get, reverse=True)]
 
 while len(Nodes) > 1:
     a = Nodes.pop()
     b = Nodes.pop()
 
-    c = Node(a.count+b.count)
+    c = HuffNode(a.count+b.count)
     c.left, c.right = a, b
 
     Nodes.append(c)
     Nodes.sort(key=lambda x: x.count, reverse=True)
-          
-print(Nodes[0].right.right.right.letter)
+
+def printcode(l, n, code = ''):
+    if n.letter == l:
+        return code
+    else:
+        try:
+            #print(n.count)
+            return printcode(l, n.left, code + '0')
+        except:
+            #print(n.count)
+            return printcode(l, n.right, code + '1')
+
+#def traversal(node, d):
+#    codes = copy.deepcopy(d)
+#    for k in d.keys():
+#        pass
+#    if node.left == None and node.right == None:
+        
+print(printcode('h', Nodes[0]))
